6955329748795d5d22897ac2c10b5099eca4399e
Aleksej <man747704@gmail.com>
2023-03-12 15:02:45 +0300

    large fixes + can detect many function calls

--- PythonTokenizer.java
+++ PythonTokenizer.java
@@ -1,5 +1,6 @@
 import org.python.util.PythonInterpreter;
 import java.io.*;
+import java.net.InterfaceAddress;
 import java.nio.file.Files;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -7,10 +8,23 @@ import java.util.Scanner;
 public class PythonTokenizer {
 
     private static final String fileWithCodePath = "C:\\Users\\Aleksej\\Desktop\\in1.py";
-    private static TokensMap tokensMap = new TokensMap();
+    private static LinkedList<TToken> tokenFlow;
+    private static int next = 0;
 
     public static void main(String[] args) {
-        tokenize(readCodeFromFile(fileWithCodePath));
+        TToken item;
+        int function_numbers = 0;
+        tokenFlow = tokenize(readCodeFromFile(fileWithCodePath));
+        for (int i = 0; i<tokenFlow.size();i++){
+            item = tokenFlow.get(i);
+            next = i;
+            if (item.tokenType()==TokenType.INNER_FUNCTION || item.tokenType()==TokenType.IDENTIFIER)
+                if (function()){
+                    System.out.println(item.tokenValue());
+                    function_numbers++;
+                }
+        }
+        System.out.println("FFF -> "+function_numbers);
     }
 
     private static String readCodeFromFile(String path){
@@ -27,8 +41,21 @@ public class PythonTokenizer {
         return res.toString();
     }
 
-    private static TokenType getTokenType(String key){
-        return tokensMap.getMap().get(key);
+    private static TokenType getTokenType(String[] lineGroup){
+        TokenType res = null;
+        switch (lineGroup[1]){
+            case "NAME"->{
+                if (lineGroup[2].matches(TokensMap.keyWords)) res = TokenType.KEY_WORD;
+                else if (lineGroup[2].matches(TokensMap.inner_functions)) res = TokenType.INNER_FUNCTION;
+                else if (lineGroup[2].matches(TokensMap.identifiers)||(lineGroup[2].matches(TokensMap.build_in_vars))) res = TokenType.IDENTIFIER;
+            }
+            case "STRING"->res = TokenType.STRING;
+            case "NUMBER"->res = TokenType.NUMBER;
+            case "OP"->res = TokenType.OPERATION;
+            case "COMMENT" -> res = TokenType.COMMENT;
+            default -> res = TokenType.UNKNOWN;
+        }
+        return res;
     }
 
     public static LinkedList<TToken> tokenize(String code) {
@@ -46,12 +73,48 @@ public class PythonTokenizer {
         while (vConsole.hasNextLine()){
             lineGroup = vConsole.nextLine().split("\t");
             lineGroup[2] = lineGroup[2].substring(1, lineGroup[2].length()-1);
-            if (lineGroup[1].compareTo("NAME") == 0) res.add(new TToken(lineGroup[2], getTokenType(lineGroup[2])));
+            res.add(new TToken(lineGroup[2], getTokenType(lineGroup)));
         }
 
-        res.forEach(item -> {System.out.println(item.tokenValue() +" "+ item.tokenType());});
+       // res.forEach(item -> {System.out.println(item.tokenValue() +" "+ item.tokenType());});
 
-        return null;
+        return res;
     }
+
+    private static boolean termOP(final String expected){
+        return tokenFlow.get(next++).tokenValue().equals(expected);
+    }
+
+    private static boolean term(final TokenType expected){
+        return tokenFlow.get(next++).tokenType() == expected;
+    }
+
+    private static boolean function(){
+        int save = next;
+        return (anyTypeOfFunction() && termOP("(") && params_enc() && termOP(")"))||
+                (retToSave(save) && anyTypeOfFunction() && termOP("(") && termOP("f") && term(TokenType.STRING) && termOP(")"));
+    }
+
+    private static boolean anyTypeOfFunction(){
+        int save = next;
+        return (term(TokenType.IDENTIFIER)) || (retToSave(save) && term(TokenType.INNER_FUNCTION));
+    }
+
+    private static boolean retToSave(final int save) {next = save; return true;}
+
+    private static boolean funct_params(){
+        int save = next;
+        return (retToSave(save) && term(TokenType.NUMBER))||
+                (retToSave(save) && term(TokenType.IDENTIFIER))||
+                (retToSave(save) && term(TokenType.STRING));
+    }
+
+    private static boolean params_enc(){
+        int save = next;
+        return (funct_params() && termOP(",") && params_enc()) ||
+                (retToSave(save) && funct_params()) ||
+                (retToSave(save) && Boolean.getBoolean("true"));
+    }
+
 }
 record TToken(String tokenValue, TokenType tokenType){}
--- TokensMap.java
+++ TokensMap.java
@@ -8,81 +8,8 @@ import java.util.regex.Pattern;
 
 public class TokensMap {
     private HashMap<String, TokenType> mainKeywords;
-
-//    public static void main(String[] args) {
-//        initMainKeywords();
-//        System.out.println(getTokensFromFile("C:\\Users\\Aleksej\\Desktop\\in.txt"));
-//    }
-
-    public TokensMap(){
-        initMainKeywords();
-    }
-
-    public HashMap<String, TokenType> getMap(){
-        return mainKeywords;
-    }
-
-    private void initMainKeywords(){
-        mainKeywords = new HashMap<>();
-        mainKeywords.put("for", TokenType.FOR);
-        mainKeywords.put("while", TokenType.WHILE);
-        mainKeywords.put("if", TokenType.IF);
-        mainKeywords.put("else", TokenType.ELSE);
-        mainKeywords.put("elif", TokenType.ELIF);
-        //mainKeywords.put("print", TokenType);
-        mainKeywords.put("None", TokenType.NONE);
-        mainKeywords.put("+", TokenType.PLUS);
-        mainKeywords.put("-", TokenType.MINUS);
-        mainKeywords.put("*", TokenType.MULTIPLICATION);
-        mainKeywords.put("/", TokenType.DIVIDE);
-        mainKeywords.put("=", TokenType.ASSIGNMENT);
-        mainKeywords.put(":", TokenType.COLON);
-        mainKeywords.put(";", TokenType.SEMICOLON);
-        mainKeywords.put("(", TokenType.LEFT_BRACE);
-        mainKeywords.put(")", TokenType.RIGHT_BRACE);
-        //mainKeywords.put(">=", TokenType.FOR.GREATER_OR_EQUALS);
-        //mainKeywords.put("<=", TokenType.FOR.LOWER_OR_EQUALS);
-        //mainKeywords.put(">", TokenType.FOR.GREATER_THAN);
-        //mainKeywords.put("<", TokenType.FOR.LOWER_THAN);
-//        mainKeywords.put("<>", TokenType.FOR.NOT_EQUALS);
-//        mainKeywords.put(":=", TokenType.FOR.ASSIGNMENT_OPERATOR);
-//        mainKeywords.put("@", TokenType.FOR.AT_SIGN);
-        mainKeywords.put("{", TokenType.LEFT_CBRACE);
-        mainKeywords.put("}", TokenType.RIGHT_CBRACE);
-    }
-
-//    public static String getTokensFromFile(String filePath){
-//        File input = new File(filePath);
-//        StringBuilder rawInput = new StringBuilder();
-//
-//        try (BufferedReader reader = Files.newBufferedReader(input.toPath())) {
-//            String line;
-//            while ((line = reader.readLine()) != null) rawInput.append(line).append('\n');
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return rawInput.toString();
-//    }
-
-    private static String lexicalAnalyzer(String rawInput){
-        Pattern operators = Pattern.compile("(?<=\s|^)(as|and|break|continue|def|elif|else|if|False|True|None|with|for|in|is|nor|not|return|while)(?=\s|:)");
-        Pattern comments = Pattern.compile("#.+");
-        Pattern identifiers = Pattern.compile("[a-zA-Z_]+[a-zA-Z0-9_]*");
-        Pattern delimiters = Pattern.compile("(\\[|\\]|\\(|\\)|:|-|\\*|\\/|\\{|\\}|\\||\\')");
-
-        Matcher m_comments = comments.matcher(rawInput);
-        rawInput = m_comments.replaceAll("");
-
-//        Matcher m_operators = operators.matcher(rawInput);
-
-        StringBuilder res = new StringBuilder();
-        StringBuilder temp = new StringBuilder();
-        for (String line: rawInput.lines().toList()){
-            for (char chr:line.toCharArray()){
-                temp.append(chr);
-            }
-        }
-
-        return res.toString();
-    }
+    static String keyWords = "(as|and|break|continue|def|elif|else|if|False|True|None|with|for|in|is|not|return|while)";
+    static String inner_functions = "(abs|bool|bytes|chr|hash|len|pow|print|sorted|type|input|int|range)";
+    static String identifiers = "[a-zA-Z_]+[a-zA-Z0-9_]*";
+    static String build_in_vars = "(__name__)";
 }
